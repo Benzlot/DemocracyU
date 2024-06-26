94e4ee8210975fcb4de86c03f30c3025aaffc9db
Khumphol Mettawasee <6452100181@stu.pim.ac.th>
2024-06-26 15:42:13 +0700

    Revert "แก้ไขใหม่"
    This reverts commit c40cb852960961baa1af4d88dad86e51af63f5ec.

--- frontend/src/components/Navbar.jsx
+++ frontend/src/components/Navbar.jsx
@@ -1,12 +1,8 @@
 import React, { useContext } from 'react';
 import { Link } from 'react-router-dom'; // Assuming you're using react-router-dom
 import { AuthContext } from '../context/AuthContext'; // Your authentication context
-<<<<<<< HEAD
 import '../components-style/Navbar.css'; // Import your CSS for styling
 import '../components-style/UserDB.css'; // Import additional CSS if needed
-=======
-import '../Components style/Navbar.css'; // Import your CSS for styling
->>>>>>> parent of b28c5af (สร้างเวลาดิจิทัล และกำลังทำ voting)
 
 const Navbar = () => {
   const { account } = useContext(AuthContext); // Assuming account contains user information if logged in

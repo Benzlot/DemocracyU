316132033cb1d9f80cf945fe4056508eacc1df62
Benzlot <benzlot961@gmail.com>
2024-07-27 12:31:22 +0700

    update msal cache storage

--- frontend/src/config/msalConfig.js
+++ frontend/src/config/msalConfig.js
@@ -2,11 +2,11 @@ export const msalConfig = {
     auth: {
       clientId: 'edaf0096-1c9b-45d9-82ed-f74550b4bd17', 
       authority: 'https://login.microsoftonline.com/f7445e93-7b8f-4bb9-9ac8-b30d33268565', 
-      redirectUri: 'https://democracy-u.netlify.app', // Replace with your redirect URI
+      redirectUri: 'https://democracyu.onrender.com', // Replace with your redirect URI
     },
     cache: {
-      cacheLocation: 'localStorage', // This configures where your cache will be stored
-      storeAuthStateInCookie: false, // Set this to true if you are having issues on IE11 or Edge
+      cacheLocation: 'sessionStorage', // This configures where your cache will be stored
+      storeAuthStateInCookie: true, // Set this to true if you are having issues on IE11 or Edge
     },
   };
   
